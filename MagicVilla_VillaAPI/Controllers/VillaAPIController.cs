6d2b97a20006ef99f6400d29baf43dae12f37d57
Antoniou <ice19390019@uniwa.gr>
2023-09-14 14:50:57 +0300

    http get methods sto villa controller

---
+++ MagicVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -0,0 +1,44 @@
+﻿using MagicVilla_VillaAPI.Data;
+using MagicVilla_VillaAPI.Models;
+using MagicVilla_VillaAPI.Models.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_VillaAPI.Controllers
+{
+    [Route("api/VillaAPI")]
+    // Tha mporousa na valw kai api/[controller]
+    // Me afto tha epairne to onoma tou controller class
+    [ApiController]
+    public class VillaAPIController : ControllerBase
+    {
+
+        [HttpGet]
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
+        {
+            return Ok(VillaStore.VillaList);
+        }
+
+        [HttpGet("{id:int}")] // perimenei ena id parameter
+        //[ProducesResponseType(200, Type = typeof(VillaDTO))]
+        //[ProducesResponseType(404)]
+        //[ProducesResponseType(400)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public ActionResult<VillaDTO> GetVilla(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var villa = VillaStore.VillaList.FirstOrDefault(u => u.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound(); // to gnwsto 404
+            }
+            return Ok(villa);
+        }
+    }
+}
---
+++ MagicVilla_VillaAPI/Models/Dto/VillaDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace MagicVilla_VillaAPI.Models.Dto
+{
+    public class VillaDTO
+    {
+        // ta DTO (data transfer object) ta exoume gia na ta xrhsimopoioume
+        // sta requests kai na mhn vazoume to kaoniko object pou exei
+        // oles tis plhrofories, edw vazoume mono oti theloume na fainetai
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+    }
+}
