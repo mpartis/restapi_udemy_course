717404c1c76cab069fd1949a84f66c28980abe7d
Giannis <73720222+mpartis@users.noreply.github.com>
2023-10-23 15:45:38 +0300

    Update APIResponse.cs (typo)

--- MagicVilla_VillaAPI/Models/APIResponse.cs
+++ MagicVilla_VillaAPI/Models/APIResponse.cs
@@ -6,7 +6,7 @@ namespace MagicVilla_VillaAPI.Models
     {
         // kserw gw apo edw kai pera ola ta endpoints tha epistrefoun
         // APIResponse kai to ekastote villa, villadto ktl tha mpainei sto
-        // object response.
+        // object result.
         // Etsi ola ta endpoints tha epistrefoun to idio type
         // Kathws etsi prepei sta sovara apis symfwna me ton indo daskalo
         public HttpStatusCode StatusCode { get; set; }
